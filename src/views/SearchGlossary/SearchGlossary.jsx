30b9c5bf811ae3f4274ac11d5b761771cd427f53
bryanpizzillo <bryanpizzillo@users.noreply.github.com>
2020-02-02 14:53:09 -0500

    This router sucks

--- src/views/SearchGlossary/SearchGlossary.jsx
+++ src/views/SearchGlossary/SearchGlossary.jsx
@@ -6,7 +6,7 @@ const SearchGlossary = ({dictionaryName}) => {
     <div data-testid="search-glossary">
       <h3>{dictionaryName}</h3>
       <h4>This is the search page container</h4>
-      <Link to='./'>Home page</Link>
+      <Link to='../'>Home page</Link>
     </div>
   );
 };
