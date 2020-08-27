0aa91a97324271d0ef5f7766ac8a14e01605bedb
karlikpj <paul.karlik@nih.gov>
2020-08-27 12:49:05 -0400

    moving the pager into the playground

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -12,11 +12,33 @@ export const i18n = {
 		es: 'No se encontró la página',
 	},
 	pleaseCheckSpellingOrTryAnotherSearch: {
-		en: 'Please check your spelling or try another search using a different word.',
-		es: 'Please check your spelling or try another search using a different word.'
+		en:
+			'Please check your spelling or try another search using a different word.',
+		es:
+			'Please check your spelling or try another search using a different word.',
 	},
 	search: {
 		en: 'Search',
 		es: 'Buscar',
+	},
+	previous: {
+		en: 'Previous',
+		es: 'Anterior',
+	},
+	next: {
+		en: 'Next',
+		es: 'Siguiente',
+	},
+	show: {
+		en: 'Show',
+		es: 'Mostrar',
+	},
+	resultsPerPage: {
+		en: 'results per page',
+		es: 'resultados por página',
+	},
+	goToPage: {
+		en: 'Go to Page',
+		es: 'Ir a la Página',
 	},
 };
