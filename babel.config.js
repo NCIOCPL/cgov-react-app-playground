d57c34053454c1657d12b85c6f52f7ab5cf7aad0
adriancofie <adriancofie@gmail.com>
2023-03-27 16:10:28 -0400

    (#90) Update Code Coverage Scripts and Config
    * Fixes coverage collection (Note: This results in a decrease in our coverage score)
        *  Updates coverage collection key to specify the src directory and exclude test files:
    ```
        "collectCoverageFrom": [
    			      "src/**/*.{js,jsx,ts,tsx}",
    			      "!src/**/*.test.{js,jsx,ts,tsx}",
    			      "!src/**/*.d.ts",
    			      "!cypress/**/*.{spec,test}.{js,jsx,ts,tsx}"
    		      ],
     ```
    
    * Modifies coverage threshold to current / as-is coverage
          (Note: The ideal minimum target for all repositories should be 80%. As we will most likely be refactoring chunks of the codebase we agreed to codify current thresholds and target between current scores and 80(+) for newly introduced code )
    
     * Updates webpack config file to add istanbul plugin on non-Prod environments
        * Removes plugin add from babel config
    
    * Moves the nyc config from package.json to nyc.config.json for visibility / consistency
    * Excludes Create React App scaffolding files from code coverage collection
    * Includes Cypress JS files in code standards checks
    
    Closes #90

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,6 @@
 const plugins = [];
-if (process.env.NODE_ENV === 'test') {
-	plugins.push(['istanbul']);
-}
 
 module.exports = {
-  presets: ['react-app'],
-  plugins
+	presets: ['react-app'],
+	plugins,
 };
