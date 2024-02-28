34d76689ceacc51af28b5833f786f0535e4e4c8f
Bela Olson <bela.olson@nih.gov>
2024-02-28 16:27:16 -0500

    (#102) Update Node, Webpack, and other security updates
    Updates node, webpack, and other packages to enable security changes. Upgrades dependencies to webpack 5 compatible versions.
    
    Package |  Old Version ->  New Version
    webpack |   ^4.42.1 ->  ^5.x
    axios	| ^0.21   -> ^1.6.x
    cypress	|^10.9. ->  ^13.x
    eslint	|^7.20.0  -> ^8.x
    start-server-and-test| ^1.11.7 -> 2.x
    webpack-manifest-plugin	|2.2.0 -> 3.x
    
    See https://github.com/NCIOCPL/cgov-react-app-playground/pull/101 for full details.
    
    Closes #102

---
+++ src/tracking/__tests__/eddl-analytics-provider.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import EddlAnalyticsProvider from '../eddl-analytics-provider';
+import { EddlAnalyticsHandler } from '../../utils/index';
+
+jest.mock('../../utils/index', () => ({
+	EddlAnalyticsHandler: jest.fn(() => jest.fn()),
+}));
+
+describe('EddlAnalyticsProvider', () => {
+	it('should render without crashing', () => {
+		const children = <div>Test</div>;
+		const analyticsHandler = EddlAnalyticsHandler(window);
+
+		render(
+			<EddlAnalyticsProvider
+				analyticsHandler={analyticsHandler}
+				pageName="testPage"
+				pageTitle="Test Page"
+				pageMetaTitle="Test Meta Title"
+				pageLanguage="English"
+				pageAudience="Test Audience"
+				pageChannel="Test Channel"
+				pageContentGroup="Test Content Group"
+				pagePublishedDate="Test Date">
+				{children}
+			</EddlAnalyticsProvider>
+		);
+
+		// Use Testing Library's query method
+		const testElement = screen.getByText('Test');
+		expect(testElement).toBeInTheDocument();
+	});
+});
